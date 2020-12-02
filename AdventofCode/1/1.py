a188a31c2ea0c22b505376f1a95f6abf16426a82
Clément Guibout <clement.guibout@gmail.com>
2020-12-02 18:41:59 +0100

    day 1 and day 2

---
+++ AdventofCode/1/1.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+from math import prod
+
+numbers = []
+with open('input') as fi:
+    for line in fi:
+        numbers.append(int(line))
+
+# Part 1
+for combi in combinations(numbers, 2):
+    if sum(combi) == 2020:
+        print(prod(combi))
+
+# Part 2
+for combi in combinations(numbers, 3):
+    if sum(combi) == 2020:
+        print(prod(combi))
