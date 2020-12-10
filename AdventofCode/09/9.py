378d0143bfb88c2d46712f03920a61d29665ad4a
Clément Guibout <clement.guibout@gmail.com>
2020-12-10 19:04:26 +0100

    day 9

---
+++ AdventofCode/09/9.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+
+with open("input") as fi:
+    file = list(map(int, fi.readlines()))
+
+# Part 1
+p1 = 0
+for line in range(25, len(file)):
+    if not any(sum(combi) == file[line] for combi in combinations(file[line-25:line], 2)):
+        p1 = file[line]
+
+# Part 2
+p2 = 0
+for line in range(len(file)):
+    for num in range(line + 2, len(file)):
+        total = sum(file[line:num])
+        if total > p1:
+            break
+        elif total == p1:
+            p2 = min(file[line:num]) + max(file[line:num])
+
+print(p1, p2)
