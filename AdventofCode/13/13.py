90ddffa745cee4c9f78c86b32b117a773470d387
Clément Guibout <clement.guibout@gmail.com>
2020-12-13 19:10:33 +0100

    day 13

---
+++ AdventofCode/13/13.py
@@ -0,0 +1,53 @@
+with open("input", "r") as fi:
+    file = [line.strip() for line in fi.readlines()]
+    depart = int(file[0])
+    buses = [int(number) if number != 'x' else 'x' for number in file[1].split(',')]
+
+# Stolen from Rosetta code
+from functools import reduce
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+# Part 1
+times = []
+
+for bus in buses:
+    if bus == "x":
+        continue
+
+    num_missed = int(depart / bus)
+    times.append((bus, (bus * (num_missed + 1)) % depart))
+
+min_wait = min(times, key=lambda t: t[1])
+p1 =  min_wait[0] * min_wait[1]
+
+# Part 2
+all_buses = []
+differences = []
+
+for index, bus in enumerate(buses):
+    if bus == "x":
+        continue
+
+    all_buses.append(bus)
+    differences.append(bus - index)
+
+p2 = chinese_remainder(all_buses, differences)
+
+print(p1, p2)
